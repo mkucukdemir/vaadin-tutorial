ab0ee693fa04558c87cf5ec065dc3a4115b59ea2
mehmetkucukdemir <mehmetkucukdemir@aburakeliacik-HP-Compaq-6000-Pro-MT-PC>
2018-06-26 18:05:05 +0300

    Table was resized and date column format was expressed.

--- BookStore/src/main/java/com/smallfe/bookstore/component/BookTable.java
+++ BookStore/src/main/java/com/smallfe/bookstore/component/BookTable.java
@@ -6,9 +6,12 @@
 package com.smallfe.bookstore.component;
 
 import com.smallfe.bookstore.db.entity.Book;
+import com.vaadin.data.Property;
 import com.vaadin.data.util.BeanContainer;
+import com.vaadin.event.ItemClickEvent;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -19,19 +22,30 @@ import java.util.List;
 public class BookTable extends VerticalLayout {
 
     public BookTable(List<Book> bookList) {
+        this.setHeight("400px");
         BeanContainer<Long, Book> books = new BeanContainer<Long, Book>(Book.class);
         books.setBeanIdProperty("id");
         
         books.addAll(bookList);
         
-        Table table = new Table("Book list of the store", books);
+        Table table = new Table("Book list of the store"){
+            @Override
+            protected String formatPropertyValue(Object rowId, Object colId, Property property) {
+                if (property.getType() == Date.class) {
+                    SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+                    return df.format((Date)property.getValue());
+                }
+                return super.formatPropertyValue(rowId, colId, property);
+            }
+        };
+        table.setContainerDataSource(books);
         table.setColumnHeader("title", "Title");
         table.setColumnHeader("author", "Author");
         table.setColumnHeader("isbn", "ISBN");
         table.setColumnHeader("type", "Type");
         table.setColumnHeader("publishDate", "Publish Date");
         table.setVisibleColumns(new Object[] {"title","author","isbn","type","publishDate"});
-        table.setHeight("200px");
+        table.setSizeFull();
         
         addComponent(table);
         
